7f0dcf35ed96fb2aae9b16bdbfc3b55153a532ba
RishavKrishnan <97791930+RishavKrishnan@users.noreply.github.com>
2022-09-04 22:14:40 +0530

    Vector

---
+++ vector.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+   // int arr[]={1,2,4,7,6,8,9};
+    vector<int> arr(10,7);
+    for(int i=0;i<=9;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+}
