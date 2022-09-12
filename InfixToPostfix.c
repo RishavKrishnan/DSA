8622dd07cb66c92dde60585a1e7ceb47bd83bfdb
RishavKrishnan <97791930+RishavKrishnan@users.noreply.github.com>
2022-09-12 21:53:54 +0530

    Infix to postfix conversion

---
+++ InfixToPostfix.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+// #define int top=-1;
+// int push()
+// {
+
+// }
+int main()
+{
+    char s[10];
+    printf("Enter the string: \n");
+    //scanf("%c",s);
+    gets(s);
+    char *e=s;
+    printf("%c",*e);
+
+    
+}
